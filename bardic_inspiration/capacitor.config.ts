0a16cc7ed1545b37ee3d7590398bfb7abd29f589
Schuster Christoph <chrisschust@gmail.com>
2024-07-21 15:29:36 +0200

    delete

--- bardic_inspiration/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'bardic_inspiration',
-  webDir: 'www'
-};
-
-export default config;
